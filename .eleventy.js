0a780dd1ecae7f2a3e6ecd280754a4b8d0eda921
Oneted11 <muriithited@gmail.com>
2022-09-21 16:17:26 +0300

    feat:added project collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,10 @@
 module.exports = function (eleventyConfig) {
+  //makes the stylesheets and images and favicons work
+  eleventyConfig.addPassthroughCopy("./src/css/");
+  eleventyConfig.addWatchTarget("./src/css/");
+  eleventyConfig.addPassthroughCopy("./src/images/");
+  eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" });
+  // specifyis watch foder and processed files dir
   return {
     dir: {
       input: "src",
