d0f0254832d99a02219b0570f878e28a61266b29
Oneted11 <muriithited@gmail.com>
2022-10-02 09:59:54 +0300

    chore:comment

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 const Image = require("@11ty/eleventy-img");
-
+//image processing shortcode
 async function imageShortcode(src, alt, sizes) {
   let metadata = await Image(`./src${src}`, {
     widths: [300, 800, null],
